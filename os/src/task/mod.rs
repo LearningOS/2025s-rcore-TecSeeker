8c19dab5c00ac3e4d6ee2123ecf3e6dfc23a50fd
seeker <seeker@TecSeeker.lan>
2025-03-29 17:44:02 +0800

    lab1 compeleted

--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -14,7 +14,7 @@ mod switch;
 #[allow(clippy::module_inception)]
 mod task;
 
-use crate::config::MAX_APP_NUM;
+use crate::config::{MAX_APP_NUM, MAX_SYSCALL_ID};
 use crate::loader::{get_num_app, init_app_cx};
 use crate::sync::UPSafeCell;
 use lazy_static::*;
@@ -54,6 +54,7 @@ lazy_static! {
         let mut tasks = [TaskControlBlock {
             task_cx: TaskContext::zero_init(),
             task_status: TaskStatus::UnInit,
+            syscall_counts: [0; MAX_SYSCALL_ID + 1],
         }; MAX_APP_NUM];
         for (i, task) in tasks.iter_mut().enumerate() {
             task.task_cx = TaskContext::goto_restore(init_app_cx(i));
@@ -135,6 +136,18 @@ impl TaskManager {
             panic!("All applications completed!");
         }
     }
+    /// get syscall counts
+    fn get_syscall_counts(&self, syscall_id: usize) -> usize{
+        let inner = self.inner.exclusive_access();
+        let current = inner.current_task;
+        inner.tasks[current].syscall_counts[syscall_id]
+    }
+    /// add syscall counts
+    fn add_syscall_counts(&self, syscall_id: usize) {
+        let mut inner = self.inner.exclusive_access();
+        let current = inner.current_task;
+        inner.tasks[current].syscall_counts[syscall_id] += 1;
+    }
 }
 
 /// Run the first task in task list.
@@ -168,4 +181,12 @@ pub fn suspend_current_and_run_next() {
 pub fn exit_current_and_run_next() {
     mark_current_exited();
     run_next_task();
+}
+/// emm
+pub  fn get_syscall_counts(syscall_id:usize) -> usize{
+    TASK_MANAGER.get_syscall_counts(syscall_id)
+}
+/// emm
+pub fn add_syscall_counts(syscall_id:usize) {
+    TASK_MANAGER.add_syscall_counts(syscall_id);
 }
--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -1,5 +1,7 @@
 //! Types related to task management
 
+use crate::config::MAX_SYSCALL_ID;
+
 use super::TaskContext;
 
 /// The task control block (TCB) of a task.
@@ -9,6 +11,8 @@ pub struct TaskControlBlock {
     pub task_status: TaskStatus,
     /// The task context
     pub task_cx: TaskContext,
+    /// store syscall counts
+    pub syscall_counts: [usize; MAX_SYSCALL_ID + 1],
 }
 
 /// The status of a task
